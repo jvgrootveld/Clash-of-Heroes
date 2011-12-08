74083955c67811910aa65600efaeba3127ab15c5
Nick van den Adel <Nick@dyn205-077.student.ac.hro.nl>
2011-12-08 12:14:16 +0100

    NewGameViewController toegevoegd samen met de bijbehorende xib file.

---
+++ ClashOfHeroes/NewGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewGameViewController.h
+//  ClashOfHeroes
+//
+//  Created by Nick Vd adel on 08-12-11.
+//  Copyright (c) 2011 Pro4all. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewGameViewController : UIViewController
+
+@end
